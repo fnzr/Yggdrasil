09bf515069e2992d130bc717066c1558a92db4cc
fnzr <dev@fnzr.xyz>
2020-11-20 11:56:49 -0300

    Log to better see how I have no idea what Im doing

--- PacketParser/Program.cs
+++ PacketParser/Program.cs
@@ -8,15 +8,15 @@ namespace PacketParser
     class Program
     {
 
-        static Unit LoginSuccess(IPEndPoint ip, Messages.Credentials creds)
+        static Unit LoginSuccess(IPEndPoint ip, Structure.Credentials creds)
         {
             //Console.WriteLine(" Login success!");
             //Console.WriteLine(creds);
-            CharacterService.SelectCharacter(ip, creds, 0, FSharpFunc<Messages.SpawnZoneInfo, Unit>.FromConverter(CharacterSelected));
+            CharacterService.SelectCharacter(ip, creds, 0, FSharpFunc<Structure.SpawnZoneInfo, Unit>.FromConverter(CharacterSelected));
             return null;
         }
 
-        static Unit CharacterSelected(Messages.SpawnZoneInfo zoneInfo)
+        static Unit CharacterSelected(Structure.SpawnZoneInfo zoneInfo)
         {
             Console.WriteLine("Character selected!");
             //Console.WriteLine(zoneInfo);
@@ -26,8 +26,9 @@ namespace PacketParser
 
         static void Main(string[] args)
         {
-            var loginServer = new IPEndPoint(IPAddress.Parse("192.168.2.10"), 6900);
-            LoginService.Authenticate(loginServer, "roboco", "111111", FSharpFunc<Tuple<IPEndPoint, Messages.Credentials>, Unit>.FromConverter(
+            //Logging.ConfigureLogger();
+            var loginServer = new IPEndPoint(IPAddress.Parse("10.20.11.41"), 6900);
+            LoginService.Authenticate(loginServer, "roboco", "111111", FSharpFunc<Tuple<IPEndPoint, Structure.Credentials>, Unit>.FromConverter(
                 t =>
                  LoginSuccess(t.Item1, t.Item2)
                 ));
