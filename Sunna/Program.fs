e2f0f9753426cf9d6b2f20a21dbb44687b64e3d7
fnzr <dev@fnzr.xyz>
2020-12-11 11:15:12 -0300

    Behavior Tree nodes with DUs

--- Sunna/Program.fs
+++ Sunna/Program.fs
@@ -3,14 +3,48 @@ open System.Diagnostics
 open System.Net
 open NLog
 open Yggdrasil
+open Yggdrasil.BehaviorTree
 open Yggdrasil.Navigation
+open Yggdrasil.Types
 
 let Logger = LogManager.GetCurrentClassLogger()
 
 let BehaviorFactory id = ()
 
+type MyState = {
+    mutable Counter: int
+}
+
+let IncreaseCounter (state: MyState) =
+    state.Counter <- state.Counter + 1
+    Status.Success
+
+let CheckCount (state: MyState) =
+    printfn "Current counter: %A" state.Counter 
+    if state.Counter < 3 then Status.Success
+    else Status.Success
+    
+let Tree = Sequence([|Leaf <| Action(CheckCount)
+                      Leaf <| Action(IncreaseCounter)
+                      Leaf <| Action(IncreaseCounter)
+                      Leaf <| Action(CheckCount)
+                      Leaf <| Action(IncreaseCounter)
+                      Leaf <| Action(CheckCount)|])
+
+let RootFun _ status _ = ActionResult.Result status
+let Root : Node<MyState> = Branch <| RootFun
+    
+let rec Run tree =
+    let state = {Counter = 0}
+    let rec run s =
+        match s with
+        | Action a -> run <| a state
+        | Result s -> printfn "Tree finished with status %A" s
+    run <| Tree [Root] Status.Initializing state 
+
 [<EntryPoint>]
 let main argv =
+    Run Tree
     //let map = Yggdrasil.Navigation.Maps.MapCacheParser()    
     //let sw = Stopwatch()
     //sw.Start()
@@ -22,10 +56,11 @@ let main argv =
     //let dispatcher = Scheduling.DispatcherFactory()
     //let tick = Scheduling.GetCurrentTick()
     //dispatcher.Post <| (tick+500u, TimedEvent tick)
-    printf "Done"
-    let loginServer = IPEndPoint  (IPAddress.Parse "127.0.0.1", 6900)
-    let (agents, login) = API.CreateServerMailboxes loginServer BehaviorFactory
-    login "roboco" "111111"
-    API.CommandLineHandler agents
+    
+    //printf "Done"
+    //let loginServer = IPEndPoint  (IPAddress.Parse "127.0.0.1", 6900)
+    //let (agents, login) = API.CreateServerMailboxes loginServer BehaviorFactory
+    //login "roboco" "111111"
+    //API.CommandLineHandler agents
     //Console.ReadKey() |> ignore 
     0 // return an integer exit code
---
+++ Yggdrasil/BehaviorTree.fs
@@ -0,0 +1,52 @@
+﻿module Yggdrasil.BehaviorTree
+
+type Status = Success | Failure | Initializing | Running
+
+type ActionResult<'T> =
+    |Action of Action<'T>
+    |Result of Status
+and Action<'T> = 'T -> ActionResult<'T>
+
+type Node<'T> =
+    | Branch of (Node<'T> list -> Status -> Action<'T>)
+    | Leaf of (Node<'T> list -> Action<'T>)
+    
+let Move<'T> (node: Node<'T>) stack status =
+    match node with
+    | Branch b -> b stack status
+    | Leaf f -> f stack
+
+let rec Sequence (children: Node<'T>[]) (stack: Node<'T> list) status =
+    match status with
+    | Initializing ->
+        let s = (Branch <| Sequence children.[1..]) :: stack
+        Move children.[0] s Initializing
+    | Failure -> Move stack.Head stack.Tail Failure
+    | Success ->
+        match children with
+        | [||] -> Move stack.Head stack.Tail Success                  
+        | remaining ->
+            let s = (Branch <| Sequence remaining.[1..]) :: stack
+            Move remaining.[0] s Initializing
+    | Running -> invalidOp "Invalid status for Sequence"
+    
+let rec Selector (children: Node<'T>[]) (stack: Node<'T> list) status =
+    match status with
+    | Initializing ->
+        let s = (Branch <| Selector children.[1..]) :: stack
+        Move children.[0] s Initializing
+    | Success -> Move stack.Head stack.Tail Success
+    | Failure ->
+        match children with
+        | [||] -> Move stack.Head stack.Tail Success                  
+        | remaining ->
+            let s = (Branch <| Selector remaining.[1..]) :: stack
+            Move remaining.[0] s Initializing
+    | Running -> invalidOp "Invalid status for Selector"
+
+let rec Action<'T> (action: 'T -> Status) (stack: Node<'T> list) state =
+    let result = action state
+    match result with
+    | Running -> ActionResult.Action (Action action stack)
+    | status -> Move stack.Head stack.Tail status state
+    
