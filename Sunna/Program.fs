7a218d386fae83b7703dd338aef5d6e3dec7c9a2
fnzr <dev@fnzr.xyz>
2021-01-27 23:01:20 -0300

    Placeholder commit

--- Sunna/Program.fs
+++ Sunna/Program.fs
@@ -4,7 +4,33 @@ open System.Net
 open Yggdrasil.Behavior
 open Yggdrasil.Game
 open Sunna.Machines
+open Yggdrasil.Game.Components
 
+let timer = new System.Timers.Timer(500.0)
+timer.AutoReset <- true
+
+// events are automatically IObservable
+let timeStream = timer.Elapsed
+
+type InventoryCell =
+    {
+        InventoryChangedEvent: Event<Inventory>
+    }
+    [<CLIEvent>]
+    member this.InventoryChanged = this.InventoryChangedEvent.Publish
+
+let InventoryCellInstance =
+    {
+      InventoryChangedEvent = Event<Inventory>()
+    }
+let a =
+    InventoryCellInstance.InventoryChanged
+    |> Observable.map (fun i -> i.Weight = i.MaxWeight)
+// Inventory -> Boolean
+let IsAtMaxWeight (e: IObservable<Inventory>) =
+    e
+    |> Observable.map (fun i -> i.Weight = i.MaxWeight)
+    
 let OnlineLogin game =
     let server = IPEndPoint (IPAddress.Parse "127.0.0.1", 6900)
     let (user, pass) = game.Credentials
